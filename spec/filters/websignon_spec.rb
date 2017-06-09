24b79fa5de74bddf351110d195854aabc717db0f
Iain Hammond <ich199@users.noreply.github.com>
2017-06-09 16:42:15 +0100

    Update websignon_spec.rb

--- spec/filters/websignon_spec.rb
+++ spec/filters/websignon_spec.rb
@@ -3,19 +3,5 @@ require 'spec_helper'
 require "logstash/filters/websignon"
 
 describe LogStash::Filters::Websignon do
-  describe "Set to Hello World" do
-    let(:config) do <<-CONFIG
-      filter {
-        wegsignon {
-          
-          message => "Hello World"
-        }
-      }
-    CONFIG
-    end
 
-    sample("message" => "some text") do
-      expect(subject.get("message")).to eq('Hello World')
-    end
-  end
 end
